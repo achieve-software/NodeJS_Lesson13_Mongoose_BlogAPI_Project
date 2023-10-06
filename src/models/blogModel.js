9ce93b9f990cec922e0267385b8e86af56717868
Achieve-software <vtcbcdtyr@gmail.com>
2023-10-06 14:14:32 +0300

    schema

--- src/models/blogModel.js
+++ src/models/blogModel.js
@@ -1,22 +1,68 @@
 "use strict";
 
-const mongoose = require("mongoose")
-const nameSchema = new mongoose.Schema({
-// _id: //auto created
-// fieldName: String //shorthand using
+const mongoose = require("mongoose");
+// const nameSchema = new mongoose.Schema(
+//   {
+//     // _id: //auto created
+//     // fieldName: String //shorthand using
 
-fieldName:{
-    type:String,
-    default:null,
-    trim:true,
-    select:true,
-    unıque:false,
-    index:false,
+//     fieldName: {
+//       type: String,
+//       default: null,
+//       trim: true,
+//       select: true,
+//       index: false,
+//       unique: false,
+//       required: [true, "error message"],
+//       enum: [[0, 1, 2, 3], "error message"],
+//       validate: [
+//         function (data) {
+//           return true;
+//         },
+//         "Error-Message",
+//       ],
+//       get: function (data) {
+//         return true;
+//       },
+//       set: function (data) {
+//         return true;
+//       },
+//     },
+//   },
 
+//   {
+//     collection: "collectionName",
+//     timestamps: true,
+//   }
+// );
+
+const blogPostSchema = new mongoose.Schema(
+  {
+    title: {
+      type: String,
+      trim: true,
+      required: true,
+    },
+    content: {
+      type: String,
+      trim: true,
+      required: true,
+    },
+
+    published: {
+      type: Boolean,
+      default: true,
+    },
+  },
+  {
+    collection: "blogPosts",
+    timestamps: true
+  }
+);
+
+ 
+
+module.exports = {
+    //BlogCategory:
+    BlogPost: mongoose.model("BlogPost", blogPostSchema)
 }
-
-
-
-
-
-},{})
