85f2c57717aff94298ce0db95f11920a8910c759
Achieve-software <vtcbcdtyr@gmail.com>
2023-10-06 15:47:26 +0300

    delete-update

--- src/controllers/blogController.js
+++ src/controllers/blogController.js
@@ -9,26 +9,52 @@ module.exports.BlogPost = {
     const data = await BlogPost.find();
     res.status(200).send({
       error: false,
-      result: data,
+      count:data.length,
+      result: data
     });
   },
 
   
   create: async (req, res) => {
     const data = await BlogPost.create(req.body);
-    res.status(200).send({
+    res.status(201).send({
       error: false,
       body: req.body,
-      result: data,
+      result: data
     });
   },
-  // read: async (req,res) => {
+  read: async (req, res) => {
 
-  // },
-  // update: async (req,res) => {
+    // req.params.postId
+    // const data = await BlogPost.findById(req.params.postId)
+    const data = await BlogPost.findOne({ _id: req.params.postId },req.body)
 
-  // },
-  // delete: async (req,res) => {
+    res.status(200).send({
+        error: false,
+        result: data
+    })
 
-  //
+},
+
+update: async (req, res) => {
+        
+  // const data = await BlogPost.findByIdAndUpdate(req.params.postId, req.body, { new: true }) // return new-data
+  const data = await BlogPost.updateOne({ _id: req.params.postId }, req.body)
+
+  res.status(202).send({
+      error: false,
+      body: req.body,
+      result: data, // update infos
+      newData: await BlogPost.findOne({ _id: req.params.postId })
+  })
+
+},
+
+delete: async (req, res) => {
+        
+  const data = await BlogPost.deleteOne({ _id: req.params.postId })
+
+  res.sendStatus( (data.deletedCount >= 1) ? 204 : 404 )
+
+},
 };
