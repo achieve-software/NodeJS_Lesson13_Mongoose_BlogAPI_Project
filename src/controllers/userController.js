14cd244d6660f1a0e893256b14ee3765828310ec
Achieve-software <vtcbcdtyr@gmail.com>
2023-10-08 18:53:51 +0300

    logout

--- src/controllers/userController.js
+++ src/controllers/userController.js
@@ -107,6 +107,14 @@ module.exports.User = {
 
 
 
+logout: async (req, res) => {
+  // Set session to null:
+  req.session = null
+  res.status(200).send({
+      error: false,
+      message: 'Logout OK'
+  })
+},
 
 
 
