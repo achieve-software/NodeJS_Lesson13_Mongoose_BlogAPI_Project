d1fbc9694d7a749595962caf99e57bc5b1471617
Achieve-software <vtcbcdtyr@gmail.com>
2023-10-08 18:06:52 +0300

    encode

--- src/models/userModel.js
+++ src/models/userModel.js
@@ -1,6 +1,7 @@
 "use strict";
 
 const mongoose = require("mongoose");
+const passwordEncrypt = require('../helpers/passwordEncrypt')
 
 const userSchema = new mongoose.Schema(
   {
@@ -9,11 +10,16 @@ const userSchema = new mongoose.Schema(
       trim: true,
       unique: true,
       required: [true, "Email field must be required."],
+      validate: [
+        (email) => (email.includes('@') && email.includes('.')), // ValidationCheck
+        'Email type is incorrect.' // If false Message.
+    ]
     },
     password: {
       type: String,
       trim: true,
       required: true,
+      set: (password) => passwordEncrypt(password)
       
     },
     firstName: String,
