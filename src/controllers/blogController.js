d329cfa7b4187af0dda9f027106ffd997fe47fdc
Achieve-software <vtcbcdtyr@gmail.com>
2023-10-08 12:37:05 +0300

    mdl

--- src/controllers/blogController.js
+++ src/controllers/blogController.js
@@ -4,61 +4,6 @@ require("express-async-errors");
 
 const {BlogCategory, BlogPost } = require("../models/blogModel");
 
-module.exports.BlogPost = {
-  list: async (req, res) => {
-    const data = await BlogPost.find();
-    res.status(200).send({
-      error: false,
-      count:data.length,
-      result: data
-    });
-  },
-
-  
-  create: async (req, res) => {
-    const data = await BlogPost.create(req.body);
-    res.status(201).send({
-      error: false,
-      body: req.body,
-      result: data
-    });
-  },
-  read: async (req, res) => {
-
-    // req.params.postId
-    // const data = await BlogPost.findById(req.params.postId)
-    const data = await BlogPost.findOne({ _id: req.params.postId },req.body)
-
-    res.status(200).send({
-        error: false,
-        result: data
-    })
-
-},
-
-update: async (req, res) => {
-        
-  // const data = await BlogPost.findByIdAndUpdate(req.params.postId, req.body, { new: true }) // return new-data
-  const data = await BlogPost.updateOne({ _id: req.params.postId }, req.body)
-
-  res.status(202).send({
-      error: false,
-      body: req.body,
-      result: data, // update infos
-      newData: await BlogPost.findOne({ _id: req.params.postId })
-  })
-
-},
-
-delete: async (req, res) => {
-        
-  const data = await BlogPost.deleteOne({ _id: req.params.postId })
-
-  res.sendStatus( (data.deletedCount >= 1) ? 204 : 404 )
-
-},
-};
-
 
 module.exports.BlogCategory = {
   list: async (req, res) => {
@@ -114,3 +59,71 @@ delete: async (req, res) => {
 
 },
 };
+
+module.exports.BlogPost = {
+  list: async (req, res) => {
+    const data = await BlogPost.find().populate("blogCategoryId");
+    res.status(200).send({
+      error: false,
+      count:data.length,
+      result: data
+    });
+  },
+
+  
+
+  listCategoryPosts: async (req, res) => {
+
+    const data = await BlogPost.find({ blogCategoryId: req.params.categoryId }).populate('blogCategoryId')
+
+    res.status(200).send({
+        error: false,
+        count: data.length,
+        result: data
+    })
+},
+  create: async (req, res) => {
+    const data = await BlogPost.create(req.body);
+    res.status(201).send({
+      error: false,
+      body: req.body,
+      result: data
+    });
+  },
+  read: async (req, res) => {
+
+    // req.params.postId
+    // const data = await BlogPost.findById(req.params.postId)
+    const data = await BlogPost.findOne({ _id: req.params.postId },req.body)
+
+    res.status(200).send({
+        error: false,
+        result: data
+    })
+
+},
+
+update: async (req, res) => {
+        
+  // const data = await BlogPost.findByIdAndUpdate(req.params.postId, req.body, { new: true }) // return new-data
+  const data = await BlogPost.updateOne({ _id: req.params.postId }, req.body)
+
+  res.status(202).send({
+      error: false,
+      body: req.body,
+      result: data, // update infos
+      newData: await BlogPost.findOne({ _id: req.params.postId })
+  })
+
+},
+
+delete: async (req, res) => {
+        
+  const data = await BlogPost.deleteOne({ _id: req.params.postId })
+
+  res.sendStatus( (data.deletedCount >= 1) ? 204 : 404 )
+
+},
+};
+
+
--- src/routes/blogRoute.js
+++ src/routes/blogRoute.js
@@ -30,4 +30,7 @@ router.route('/post/:postId')
     .put(BlogPost.update)
     .delete(BlogPost.delete)
 
+    
+router.get('/category/:categoryId/posts', BlogPost.listCategoryPosts)
+
 module.exports = router
